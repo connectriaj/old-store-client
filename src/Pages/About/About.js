b74588e14ad21fe4705d07859508170ee0917c24
connectriaj <connect.riaj@hotmail.com>
2022-12-01 14:01:32 +0600

    about section updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <section>
-            <h1>About Old Store</h1>
-        </section>
-    );
+  return (
+    <section>
+      <h1 className="text-center text-4xl underline mt-20">
+        We’re All About <span className="text-primary">Smartphones</span>
+      </h1>
+      <p className="lg:w-1/2 mx-auto mt-10">
+        When one of 3 friends wanted to buy the latest iPhone, he tried to sell
+        off his then-current Samsung phone for funding but found it difficult to
+        do so.
+        When one of 3 friends wanted to buy the latest iPhone, he tried to sell
+        off his then-current Samsung phone for funding but found it difficult to
+        do so.
+        When one of 3 friends wanted to buy the latest iPhone, he tried to sell
+        off his then-current Samsung phone for funding but found it difficult to
+        do so.
+        When one of 3 friends wanted to buy the latest iPhone, he tried to sell
+        off his then-current Samsung phone for funding but found it difficult to
+        do so.
+        When one of 3 friends wanted to buy the latest iPhone, he tried to sell
+        off his then-current Samsung phone for funding but found it difficult to
+        do so.
+      </p>
+    </section>
+  );
 };
 
 export default About;
